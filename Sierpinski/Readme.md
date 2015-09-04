002adb2fb9b0b728b00d6f807e6f2562e9966bf5
DasFranck <dasfranck@gmx.com>
2015-09-04 05:20:31 +0000

    Import from GitHub

---
+++ Sierpinski/Readme.md
@@ -0,0 +1,21 @@
+# Sierpinski
+Sierpinski's traingle drawer using the turtle lib.
+
+## Features
+* Cursor may be hided.
+* Modifiable size.
+* Orange/black theme.
+
+## Changelog
+### Version 1.2 (03/09/2015)
+* Iteration modifier added.
+* Speed modifier added.
+
+### Version 1.1 (03/09/2015)
+* Hide cursor option added.
+* Orange theme option added.
+* Size modifier option added.
+* Verbosity option added (Just saying coordinates for now).
+
+### Version 1.0 (02/09/2015)
+* Initial Release.
---
+++ Sierpinski/Sierpinski.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+
+## Name:    Sierpinski.py
+## Desc:    Sierpinski's traingle drawer using the turtle lib.
+##
+## Author:  "Das" Franck Hochstaetter
+## Version: V1.2 (03/09/2015)
+
+import argparse
+from turtle import *
+from time import sleep
+from math import sqrt
+
+parser = argparse.ArgumentParser(description="Sierpinski's traingle drawer.");
+parser.add_argument("-c", "--cursor", help="show the cursor", action="store_true");
+parser.add_argument("-i", "--iteration", help="modify the number of iterations", default=4, type=int);
+parser.add_argument("-o", "--orange", help="better colors", action="store_true");
+parser.add_argument("-s", "--size", help="modify triangle size", default=400, type=int);
+parser.add_argument("--speed", help="modify the cursor speed (see turtle.speed for more informations)", default=6, type=int);
+parser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true");
+args = parser.parse_args();
+
+def main():
+    print("\033c");
+
+    if (args.orange):
+        bgcolor("black");
+        color("orange");
+    if (args.cursor == 0):
+        ht();
+
+    size = args.size;
+    it = args.iteration;
+    speed(args.speed);
+    xa = (-size)/2;
+    ya = (-size)/2;
+    xb = size/2;
+    yb = (-size)/2;
+    xc = 0;
+    yc = sqrt(3)/2 * size - size/2;
+    draw(xa, ya, xb, yb, xc, yc, it);
+    input("Push Enter to exit the program");
+
+def draw(xa, ya, xb, yb, xc, yc, it):
+    if (args.verbose):
+        print(xa, ya, xb, yb, xc, yc);
+    if (it != 0):
+        pu();
+        setpos(xa, ya);
+        pd();
+        setpos(xb, yb);
+        setpos(xc, yc);
+        setpos(xa, ya);
+        draw(xa, ya, (xb - xa) / 2 + xa, yb, (xc - xa) / 2 + xa, (yc - ya) / 2 + ya, it - 1);
+        draw((xb - xa) / 2 + xa, yb, xb, yb, xb - (xb - xc) / 2 , (yc - yb) / 2 + ya, it - 1);
+        draw((xc - xa) / 2 + xa, (yc - ya) / 2 + ya, xb - (xb - xc) / 2, (yc - yb) / 2 + ya, xc, yc, it - 1);
+
+if __name__ == '__main__':
+    main();
