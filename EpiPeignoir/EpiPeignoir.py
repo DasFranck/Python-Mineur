ac75036022ae9db83c483f4c923397fd0fbc3bb2
hochst_a <dasfranck@gmx.com>
2015-12-19 19:15:38 +0000

    V0.2

--- EpiPeignoir/EpiPeignoir.py
+++ EpiPeignoir/EpiPeignoir.py
@@ -5,12 +5,12 @@
 ## Desc:    Epitech Ranking by GPA
 ##
 ## Author:  "Das" Franck Hochstaetter
-## Version: v0.1 (14/12/2015)
+## Version: v0.2 (19/12/2015)
 ##
 ## Dependencies : - requests (pip install requests)
 ##    Requests is an Apache2 Licensed HTTP library, written in Python, for human beings.
 ##                - pandas (pip install pandas)
-##    pandas is a library providing data structures and data analysis tools for Python
+##    pandas is a library providing data structures and data analysis tools for Python.
 
 import argparse
 import pandas
@@ -25,6 +25,11 @@ re_gpa = re.compile("G.P.A.+\n.+?(\d*\.*\d+).+", re.MULTILINE)
 def main():
     parser = argparse.ArgumentParser(description="Epitech Ranking by GPA")
     parser.add_argument("-c", "--configfile", help="path to the config file", default="config.ini")
+    parser.add_argument("-l", "--loginlist", help="path to the login list")
+    parser.add_argument("-o", "--output", help="path to the txt output file")
+    parser.add_argument("--csv", help="path to the csv output file")
+    parser.add_argument("-q", "--quiet", help="Don't display anything except errors", action="store_true")
+    parser.add_argument("-n", "--noresult", help="Don't display end result", action="store_true")
     args = parser.parse_args()
     df = pandas.DataFrame()
     EPITECH_INTRA_URL = "https://intra.epitech.eu/"
@@ -35,8 +40,17 @@ def main():
             "password": config["Credential"]["password"],
             "submit": "Connect"}
 
-    #login_list = re.findall(r"([\w-]+)\n*", open("ll.txt").read())
-    login_list = re.findall(r"([\w-]+)\n*", open("pso2019").read())
+    login_list = ""
+    if (args.loginlist):
+        login_list = re.findall(r"([\w-]+)\n*", open(args.loginlist).read())
+    else:
+        while True:
+            try:
+                line = input()
+                login_list += line + "\n"
+            except:
+                break
+        login_list = re.findall(r"([\w-]+)\n*", login_list)
     for (i, login) in enumerate(login_list):
         r = requests.post(EPITECH_INTRA_URL + "user/" + login + "/", data=data)
         if (r.status_code == 403 or r.status_code == 404):
@@ -45,15 +59,21 @@ def main():
             html_page = r.text
             credits = float(re.search(re_credits, html_page).groups()[0]) if re.search(re_credits, html_page) else -42.0
             gpa = float(re.search(re_gpa, html_page).groups()[0]) if re.search(re_gpa, html_page) else -42.0
-            print("%d/%d %s %s %s" % (i+1, len(login_list), login, gpa, credits))
+            if not (args.quiet):
+                print("%d/%d %s %s %s" % (i+1, len(login_list), login, gpa, credits))
             df.set_value(len(df), "login", login)
             df.set_value(len(df)-1, "gpa", gpa)
             df.set_value(len(df)-1, "credits", credits)
     df.sort_values(["gpa", "credits"], ascending=False, inplace=True)
     df.reset_index(inplace=True, drop=True)
     df.index += 1
-    df = df.to_string()
-    print(df)
+    df_str = df.to_string()
+    if not (args.noresult or args.quiet):
+        print(df_str)
+    if (args.output):
+        open(args.output, "w").write(df_str + "\n")
+    if (args.csv):
+        df.to_csv(args.csv, encoding="utf-8", sep=";")
 
 if __name__ == "__main__":
     main()
---
+++ EpiPeignoir/Readme.md
@@ -0,0 +1,25 @@
+# EpiPeignoir
+Epitech Ranking by GPA
+
+## Features
+* Rank a list of logins by GPA:
+  * Input: stdin or txtfile
+  * Output: txt, csv (TBD) or just display on stdout
+* Get and display GPA/credits by login.
+
+## Requirements
+* requests (pip install requests)  
+    Requests is an Apache2 Licensed HTTP library, written in Python, for human beings.
+* pandas (pip install pandas)  
+    pandas is a library providing data structures and data analysis tools for Python.
+  
+  All those requirements can be easily installed by doing ``pip install -r requirements.txt``
+
+## Changelog
+### Version 0.2 (19/12/2015)
+* Stdin input added.
+* Csv output added.
+* Txt output added.
+* Silent and NoResult options added.
+### Version 0.1 (14/12/2015)
+* Initial release.
