91230a06e61a7c128e7081344432872c82ed8273
hochst_a <dasfranck@gmx.com>
2016-04-14 16:53:23 +0200

    Adding License and Filling the python file

---
+++ WeechatLogStats/README.md
@@ -0,0 +1,10 @@
+# WeeChat Log Stats
+Parse Weechat logs and generate statistics and graphs.
+
+## Usage
+
+## Options
+
+## Changelog
+
+Inspired by [kabaka's Ruby program](https://github.com/kabaka/weechat-log-stats).
--- WeechatLogStats/WeechatLogStats.py
+++ WeechatLogStats/WeechatLogStats.py
@@ -1,9 +1,39 @@
 #!/usr/bin/env python
 
 import argparse
+import rrdtool
+from datetime import datetime
+from os.path import basename
+
+class WLS:
+    def __init__(self, args):
+        self.now = datetime.now()
+
+        # Time of the first and last message on the logs
+        self.start_time = 0
+        self.end_time = 0
+
+        # Get channel and network names from filename
+        network = basename(args.file).split('.')[1]
+        channel = basename(args.file).split('.')[2]
+
+        # Init Stats
+        self.stats = {}
+        self.nick_stats = {}
+        self.emotes_stats = {}
+        self.domains_stats = {}
+        self.words_stats = {}
+
+        fd_log = open(args.file, "w")
+        content = fd_log.read()
 
 def main():
-    return 0
+    parser = argparse.ArgumentParser(description="Nothing til now")
+    parser.add_argument("file", help="Path to the log file", type=str)
+    args = parser.parse_args()
+
+    wls = WLS(args)
+    return (0)
 
 if __name__ == "__main__":
     main()
