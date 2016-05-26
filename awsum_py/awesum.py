1d2bf0d14aca7eb1e531a8aa1d54aed9743bf9a9
hochst_a <dasfranck@gmx.com>
2016-05-26 12:53:46 +0200

    Adding the class awsum with self-init, exception for incorrect and array of awsum

--- awsum_py/awesum.py
+++ awsum_py/awesum.py
@@ -20,23 +20,48 @@ from argparse import ArgumentParser         # ArgumentParser
 DESCRIPTION = "Automatic Website Status and Uptime Monitoring"
 
 
-def check_status(website):
-    url = urlparse(website)
-    status = HTTPConnection(url[1])
-    try:
-        status.request("HEAD", url[2])
-        return (status.getresponse().status)
-    except socket.error:
-        return (-1)
+# The main class (one by website)
+class awsum():
+    target = ""
+    reference = []
+    timespan = 0
+
+    # Init the awsum class with the config file (as a string), the argument namepace
+    #   and the index of his website in the target/timespan array
+    def __init__(self, config, args, i):
+        # To be modified with args
+        self.target = config["target"][i]
+        self.timespan = config["timespan"][i]
+        self.reference = config["reference"]
+
+        #if target.length != timespan.length:
+        #    except
+
+    def check_network_status(self):
+        url = urlparse(website)
+        status = HTTPConnection(url[1])
+        try:
+            status.request("HEAD", url[2])
+            return (status.getresponse().status)
+        except socket.error:
+            return (self.check_network_status())
+
 
 
 def main():
     parser = ArgumentParser(description=DESCRIPTION)
     parser.add_argument("-c", "--configfile", help="path to the config file", default="config.hjson")
     args = parser.parse_args()
-    config_file = hjson.load(open(args.configfile))
-    print(check_status(config_file["target"][0]))
+    config = hjson.load(open(args.configfile))
 
+    awsum_array = []
+    try:
+        for i in range(len(config["target"])):
+            awsum_array.append(awsum(config, args, i))
+            print(awsum_array[i].target)
+    #If a value was missing in the config file
+    except KeyError as missing_key:
+        print("Incorrect hjson config file, %s is missing." % missing_key)
 
 if __name__ == "__main__":
     main()
