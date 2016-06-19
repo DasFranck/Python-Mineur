b141c6cd5ab0bf4d90078cbbfcf085395407e8db
DasFranck <dasfranck@gmx.com>
2016-06-19 06:54:18 +0000

    Synchronous -> Asynchronous \o/ (but with a shitload of bugs)

--- awsum_py/Readme.md
+++ awsum_py/Readme.md
@@ -1,3 +1,3 @@
 # Automatic Website Status and Uptime Monitoring powered by PYthon
 
-
+Because of some functions using UNIX signals (BaseEventLoop.add_signal_handler from asyncio module), the code doesn't work on Windows or other non-UNIX systems.
--- awsum_py/awsum.py
+++ awsum_py/awsum.py
@@ -7,17 +7,22 @@
 ## Author:  "Das" Franck Hochstaetter
 ## Version: v0.X (XX/XX/201X)
 ##
-## Dependencies : - json (pip install hjson)
+## Dependencies : - hjson
 ##    A configuration file format for humans.
 ##    Relaxed syntax, fewer mistakes, more comments.
+##                - aiohttp
+##    Http client/server for asyncio.
+
+## TODO : Find a better way to end the Futures/Tasks
 
 import asyncio                              # Coroutine/Asynchronous routine
-import aiohttp
+import aiohttp                              # asyncio's http client
 import hjson                                # Config File
-import socket                               # Except socket.error
-from http.client import HTTPConnection      # HTTPConnection
-from urllib.parse import urlparse           # URLParser
+import signal
+import sys
+import warnings
 from argparse import ArgumentParser         # ArgumentParser
+from concurrent.futures import CancelledError
 
 DESCRIPTION = "Automatic Website Status and Uptime Monitoring"
 
@@ -29,41 +34,55 @@ class awsum():
     #   and the index of his website in the target/timespan array
     def __init__(self, config, i):
         try:
-            self.target = config["target"][i]
-            self.timespan = config["timespan"][i]
-            self.reference = config["reference"]
+            self.target = config["targets"][i]
+            self.timespan = config["timespans"][i]
+            self.references = config["references"]
         except KeyError as missing_key:
             raise self.InvalidConfigFile("Incorrect hjson config file, %s is missing." % missing_key)
 
-        if len(config["target"]) != len(config["timespan"]):
+        if len(config["targets"]) != len(config["timespans"]):
             raise self.InvalidConfigFile("The array of target and the array of timespan must have the same length.")
+        print("%s added." % self.target)
 
     # Launch the monitoring
     async def start(self):
-        #I should find something less ugly
+        #I should find something less ugly but hey, if it doesn't wreck the CPU that should be fine.
         while True:
             #Check if the website is up, if not check the network.
-            if (await check_website_status() == False):
-                if (await check_network_status() == False):
+            if (await self.check_website_status() == False):
+                if (await self.check_network_status() == False):
                     print("Network is down.")
                     ## Add Network_Down
                 else:
-                    print("Website is up.")
-                    ## Add Website_Up
+                    print("%s is down." % self.target)
+                    ## Add Website_Down
             else:
-                print("Website is down.")
-                ## Add Website_Down
-            await asyncio.sleep(self.timespan)           ##Check this one day
+                print("%s is up." % self.target)
+                ## Add Website_Up
+            await asyncio.sleep(self.timespan)
 
-    # Check if the network is up or down (True for Up, False for Down)
+    # Check if the website is up or down (True for Up, False for Down)
+    async def check_website_status(self):
+        async with aiohttp.ClientSession() as session:
+            try:
+                async with session.get(self.target) as response:
+                    if response.status == 200:
+                        return (True)
+                    else:
+                        return (False)
+            except aiohttp.errors.ClientOSError:
+                return (False)
+
+    # Check if the network by checking references websites is up or down (True for Up, False for Down)
     async def check_network_status(self):
-        url = urlparse(self.website)
-        status = HTTPConnection(url[1])
-        try:
-            status.request("HEAD", url[2])
-            return (1)
-        except socket.error:
-            return (0)
+        for ref in self.references:
+            async with aiohttp.ClientSession() as session:
+                try:
+                    async with session.get(ref) as response:
+                        return (True)
+                except aiohttp.errors.ClientOSError:
+                    pass;
+        return (False)
 
 
     # My own exception for an invalid Config File
@@ -71,22 +90,48 @@ class awsum():
         def __init__(self, *args, **kwargs):
             Exception.__init__(self, *args, **kwargs)
 
-
+#Signal handler
+async def exit_sigint(signame, loop):
+    print("%s catched, exiting..." % signame)
+    for task in asyncio.Task.all_tasks():
+        task.cancel()
 
 def main():
+    # I hate this warning, so made it quiet
+    warnings.filterwarnings("ignore", "coroutine 'exit_sigint' was never awaited")
+
+    #Argument parsing
     parser = ArgumentParser(description=DESCRIPTION)
     parser.add_argument("-c", "--configfile", help="path to the config file", default="config.hjson")
     args = parser.parse_args()
+
+    #Condifguration parsing
     config = hjson.load(open(args.configfile))
 
     awsum_array = []
     try:
-        for i in range(len(config["target"])):
-            awsum_array.append(awsum(config, i))
-            print(awsum_array[i].target)
-    #If a value was missing in the config file
+        for i in range(len(config["targets"])):
+            awsum_array.append(asyncio.ensure_future(awsum(config, i).start()))
+    #Except if a value was missing in the config file
     except awsum.InvalidConfigFile as err_msg:
         print("Error: IncorrectConfigFile: %s" % err_msg)
+
+    #Create the asyncio loop
+    loop = asyncio.get_event_loop()
+    #Adding signal handlers 
+    loop.add_signal_handler(getattr(signal, "SIGINT"), asyncio.ensure_future, exit_sigint("SIGINT", loop))
+    loop.add_signal_handler(getattr(signal, "SIGTERM"), asyncio.ensure_future, exit_sigint("SIGTERM", loop))
+    try:
+        #I don't remmember
+        while True:
+            result = loop.run_until_complete(asyncio.gather(*awsum_array))
+    # I don't care about those "errors"
+    except CancelledError:
+        pass
+#        logging.info('CancelledError')
+    loop.close()
+    sys.exit(0)
+
 
 if __name__ == "__main__":
     main()
