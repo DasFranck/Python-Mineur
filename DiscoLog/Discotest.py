221b2e05f73db7872ace5bfda17d9d8fe3f68b93
DasFranck Hochstaetter <dasfranck@gmx.com>
2016-11-09 00:25:38 +0100

    Cleaning Discotest

--- DiscoLog/Discotest.py
+++ DiscoLog/Discotest.py
@@ -28,54 +28,10 @@ client = discord.Client()
 logger = Logger.Logger()
 
 
-async def get_chat_logs():
+async def get_logs_bt(client, channel):
     if not (os.path.exists("chat_logs")):
         os.makedirs("chat_logs")
 
-    summary = open("chat_logs/summary.txt", 'w')
-    bar = progressbar.ProgressBar(redirect_stdout=True,
-                                  widgets=[progressbar.Percentage(), " ",
-                                           progressbar.Bar(), ' [', progressbar.Timer(), ']', ])
-
-    for chan in bar(client.private_channels):
-        log_file = open("chat_logs/" + chan.id + ".log", 'w')
-
-        recipients = chan.me.name + ", "
-        for recipient in chan.recipients:
-            recipients += recipient.name + ("" if recipient is chan.recipients[-1] else ", ")
-        if (chan.type == discord.ChannelType.group):
-            print("Fetching messages from the private channel " + chan.name + "\"")
-        else:
-            print("Fecthing messages from a chat with " + recipients)
-        bar.update()
-
-        # Get all messages
-        messages = []
-        async for item in client.logs_from(chan, limit=sys.maxsize):
-            messages.append(item)
-
-        # Make the header
-        header = "ID: %s\n" % chan.id
-        header += "Recipients:" + recipients + "\n"
-        if (chan.type == discord.ChannelType.group):
-            header += "Chan name: " + chan.name + "\n"
-        header += chan.created_at.strftime("Created at: %A %d %b %Y %H:%M:%S UTC\n")
-        header += "Length: %d messages\n\n" % len(messages)
-
-        # Write the header in the summary and in the chat log file
-        log_file.write(header)
-        summary.write(header)
-
-        # Yeah I know the limit is maybe a bit too higher
-        for msg in reversed(messages):
-            log_file.write(msg.timestamp.strftime("%Y-%m-%d %H:%M:%S\t"))
-            log_file.write(msg.author.name + "\t")
-            log_file.write(msg.content + "\n")
-        log_file.close()
-    summary.close()
-
-
-async def get_logs_bt(client, channel):
     log_file = open("chat_logs/BREAKTEST.log", 'w')
     i = 0
     messages = []
