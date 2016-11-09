3f180ed37aad6069d4232de4898ba6823cf0ea9e
DasFranck Hochstaetter <dasfranck@gmx.com>
2016-11-09 14:32:44 +0100

    Plotify is going POO, adding some plots and html generation

--- DiscoLog/plotify.py
+++ DiscoLog/plotify.py
@@ -7,8 +7,16 @@ import operator
 import os
 import plotly
 import plotly.graph_objs as go
+import re
+from collections import Counter, OrderedDict
 from datetime import date, datetime, timedelta as td
 from yattag import Doc
+
+
+# UTILS
+def generate_plot(graph_dict, file_path):
+    plotly.offline.plot(graph_dict, filename=file_path, show_link=False, auto_open=False)
+    return (plotly.offline.plot(graph_dict, filename=file_path, show_link=False, auto_open=False, output_type="div"))
 
 
 def cumultative_sum(values, start=0):
@@ -17,67 +25,155 @@ def cumultative_sum(values, start=0):
         yield start
 
 
-def get_date_array():
-    d1 = date(2016, 9, 7)
-    d2 = datetime.today().date()
-    delta = d2 - d1
+# Plotify class
+class Plotify():
+    def __init__(self, args):
+        if not (os.path.exists("plots")):
+            os.makedirs("plots")
+        self.get_date_array()
+        self.get_log_content(args)
+        self.counts = [self.chat_log.count(x) for x in self.date_array]
+        self.cumul = list(cumultative_sum(self.counts))
 
-    date_array = []
-    for i in range(delta.days):
-        date_array.append(d1 + td(days=i))
+    def get_date_array(self):
+        d1 = date(2016, 9, 7)
+        d2 = datetime.today().date()
+        delta = d2 - d1
 
-    return [x.strftime("%Y-%m-%d") for x in date_array]
+        date_array = []
+        for i in range(delta.days):
+            date_array.append(d1 + td(days=i))
+        self.date_array = [x.strftime("%Y-%m-%d") for x in date_array]
+
+    def get_log_content(self, args):
+        text = ""
+        meta_list = []
+        with open(args.log_path, "r") as file:
+            for line in file:
+                text += line
+                if (len(line.split("\t")) > 1):
+                    (date, author, _) = line.split("\t")
+                    meta_list.append((date, author))
+        self.chat_log = text
+        self.meta_list = meta_list
+        print("OK")
+
+    def plotify(self):
+        self.plots = OrderedDict()
+        self.plots["msgperday"] = (plot_msgperday(self, "PlotBT-msg.html"), "PlotBT-msg.html", "Number of messages per day")
+        self.plots["msgcumul"] = (plot_msgcumul(self, "PlotBT-msgcumul.html"), "PlotBT-msgcumul.html", "Number of cumulatives messages")
+        self.plots["top10"] = (plot_usertopx(self, 10, "PlotBT-top10.html"), "PlotBT-top10.html", "Number of cumulatives messages for the Top 10 users")
+        self.plots["top20"] = (plot_usertopx(self, 20, "PlotBT-top20.html"), "PlotBT-top20.html", "Number of cumulatives messages for the Top 20 users")
+        self.stats = OrderedDict()
+        self.stats["top10perday"] = top10_per_day(self)
+        self.stats["top10yesterday"] = top10_yesterday(self)
+
+    def write_main_html(self):
+        doc, tag, text = Doc().tagtext()
+
+        with tag('html'):
+            with tag('head'):
+                doc.asis('<meta http-equiv="content-type" content="text/html; charset=utf-8" />')
+            with tag('body'):
+                with tag('h1'):
+                    text("DiscoLog Monitoring for BreakTime (#discussion)")
+                # Standing of yesterday
+                with tag('h2'):
+                    text("Standings of yesterday")
+                with tag('h3'):
+                    doc.asis(self.stats["top10yesterday"])
+                # Graphs
+                with tag('h2'):
+                    text("Graphs")
+                doc.asis("<ul style=\"list-style-type:none\">")
+                for _, plot in self.plots.items():
+                    doc.asis("<li>")
+                    with tag('a', href=plot[1]):
+                        text(plot[2])
+                    doc.asis("</li>")
+                doc.asis("</ul>")
+                # Stats
+                with tag('h2'):
+                    text("Stats")
+                doc.asis("<ul style=\"list-style-type:none\">")
+                for _, stat in self.stats.items():
+                    doc.asis("<li>")
+                    with tag('a', href=stat[1]):
+                        text(stat[2])
+                    doc.asis("</li>")
+                doc.asis("</ul>")
+                # Footer
+                text("Page generated at %s" % datetime.now().strftime("%T the %F"))
+
+        result = doc.getvalue()
+        with open("plots/index.html", "w") as file:
+            file.write(result)
+
+    def write_standing_history_html(self):
+        doc, tag, text = Doc().tagtext()
+
+        with tag('html'):
+            with tag('head'):
+                doc.asis('<meta http-equiv="content-type" content="text/html; charset=utf-8" />')
+            with tag('body'):
+                doc.asis(self.stats["top10perday"])
+                doc.asis("<br />")
+                doc.asis("<br />")
+                text("Page generated at %s" % datetime.now().strftime("%T the %F"))
+
+        result = doc.getvalue()
+        with open("plots/standinghistory.html", "w") as file:
+            file.write(result)
 
 
-def get_log_content(args):
-    text = ""
-    meta_list = []
-    with open(args.log_path, "r") as file:
-        for line in file:
-            text += line
-            if (len(line.split("\t")) > 1):
-                (date, author, _) = line.split("\t")
-                meta_list.append((date, author))
-
-    return (meta_list, text)
-
-
-def generate_plot(graph_dict, file_path):
-    plotly.offline.plot(graph_dict, filename=file_path, show_link=False, auto_open=False)
-    return (plotly.offline.plot(graph_dict, filename=file_path, show_link=False, auto_open=False, output_type="div"))
-
-
-def plot_1(date_array, msg_counts, cumul):
+def plot_msgperday(plotify, path):
     msg_average = []
-    for i, cum in enumerate(cumul):
+    for i, cum in enumerate(plotify.cumul):
         msg_average.append(int(cum / (i + 1)))
 
-    line1 = go.Bar(x=date_array,
-                   y=msg_counts,
+    line1 = go.Bar(x=plotify.date_array,
+                   y=plotify.counts,
                    name="Messages per day")
 
-    line2 = go.Scatter(x=date_array,
+    line2 = go.Scatter(x=plotify.date_array,
                        y=msg_average,
                        name="Average messages per day")
 
     return (generate_plot({"data": [line1, line2],
                            "layout": go.Layout(title="Number of messages per day in #discussion (BreakTime)")},
-                          "plots/PlotBT-msg.html"))
+                          "plots/" + path))
 
 
-def plot_2(date_array, cumul):
-    return (generate_plot({"data": [go.Scatter(x=date_array, y=cumul)],
+def plot_msgcumul(plotify, path):
+    return (generate_plot({"data": [go.Scatter(x=plotify.date_array, y=plotify.cumul)],
                            "layout": go.Layout(title="Number of cumulatives messages in #discussion (BreakTime)")},
-                          "plots/PlotBT-msgcumul.html"))
+                          "plots/" + path))
 
 
-def top10_per_day(meta_list):
+def plot_usertopx(plotify, max, path):
+    top = Counter(elem[1] for elem in plotify.meta_list).most_common(max)
+    top_users = [elem[0] for elem in top]
+    users_line = []
+    for i, user in enumerate(top_users):
+        print(i + 1)
+        counts = [len(re.findall(r'%s.+\t%s' % (re.escape(x), re.escape(user)), plotify.chat_log)) for x in plotify.date_array]
+        cumul = list(cumultative_sum(counts))
+        line = go.Scatter(x=plotify.date_array,
+                          y=cumul,
+                          name=user)
+        users_line.append(line)
+    return (generate_plot({"data": users_line,
+                           "layout": go.Layout(title="Number of cumulatives messages for the Top %d users in #discussion (BreakTime)" % max)},
+                          "plots/" + path))
+
+
+def top10_per_day(plotify):
     # user_list = sort(list(set(([b for a,b in meta_list]))))
     text = "<pre>"
-    meta_list = [(meta[0].split(" ")[0], meta[1]) for meta in meta_list]
+    meta_list = [(meta[0].split(" ")[0], meta[1]) for meta in plotify.meta_list]
     meta_sorted = sorted(meta_list, key=operator.itemgetter(0))
     meta_grouped = [list(group) for key, group in itertools.groupby(meta_sorted, operator.itemgetter(0))]
-    for meta_per_date in meta_grouped:
+    for meta_per_date in reversed(meta_grouped[:-1]):
         count_map = {}
         for t in meta_per_date:
             count_map[t[1]] = count_map.get(t[1], 0) + 1
@@ -86,29 +182,28 @@ def top10_per_day(meta_list):
         text += "<b>" + meta_per_date[0][0] + "</b><br />"
         for (i, elem) in enumerate(top_list):
             text += "%d.\t%d\t%s<br />" % (i + 1, elem[1], html.escape(elem[0]))
-        text += "<br />"
+        if meta_per_date is not meta_grouped[0]:
+            text += "<br />"
     text += "</pre>"
     return (text)
 
 
-def write_html(pl1, pl2, tp1):
-    doc, tag, text = Doc().tagtext()
+def top10_yesterday(plotify):
+    # user_list = sort(list(set(([b for a,b in meta_list]))))
+    text = "<ol type=\"1\">"
+    meta_list = [(meta[0].split(" ")[0], meta[1]) for meta in plotify.meta_list]
+    meta_sorted = sorted(meta_list, key=operator.itemgetter(0))
+    meta_grouped = [list(group) for key, group in itertools.groupby(meta_sorted, operator.itemgetter(0))]
+    meta_yesterday = meta_grouped[-2]
+    count_map = {}
+    for t in meta_yesterday:
+        count_map[t[1]] = count_map.get(t[1], 0) + 1
+    top_list = sorted(count_map.items(), key=operator.itemgetter(1), reverse=True)[0:10]
 
-    with tag('html'):
-        with tag('head'):
-            doc.asis('<meta http-equiv="content-type" content="text/html; charset=utf-8" />')
-        with tag('body'):
-            with tag('h1'):
-                text("DiscoLog Monitoring for BreakTime")
-            doc.asis(pl1)
-            doc.asis(pl2)
-            with tag('p'):
-                doc.asis(tp1)
-            text("html generated at %s" % datetime.now().strftime("%T the %F"))
-
-    result = doc.getvalue()
-    with open("plots/index.html", "w") as file:
-        file.write(result)
+    for (i, elem) in enumerate(top_list):
+        text += "<li><pre>%d\t%s</pre></li>" % (elem[1], html.escape(elem[0]))
+    text += "</ol>"
+    return (text)
 
 
 def main():
@@ -117,19 +212,10 @@ def main():
     parser.add_argument("log_path")
     args = parser.parse_args()
 
-    if not (os.path.exists("plots")):
-        os.makedirs("plots")
-
-    date_array = get_date_array()
-    meta_list, chat_log = get_log_content(args)
-
-    counts = [chat_log.count(x) for x in date_array]
-    cumul = list(cumultative_sum(counts))
-
-    pl1 = plot_1(date_array, counts, cumul)
-    pl2 = plot_2(date_array, cumul)
-    tp1 = top10_per_day(meta_list)
-    write_html(pl1, pl2, tp1)
+    plotify = Plotify(args)
+    plotify.plotify()
+    plotify.write_main_html()
+    plotify.write_standing_history_html()
 
 
 if __name__ == '__main__':
